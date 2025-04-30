74e04cacf26cbaeae974d39feb28ed7ac6250ac7
ldsooo <uprise4@naver.com>
2025-04-30 18:29:05 +0900

    [임동수] - 새로운 서블릿 클래스 추가

---
+++ src/main/java/com/marondal/selvlet/ex/Ex05Controller.java
@@ -0,0 +1,14 @@
+package com.marondal.selvlet.ex;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex05Controller extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+
+}
